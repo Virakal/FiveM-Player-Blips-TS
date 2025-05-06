88e294a87212c490c5654961fa6ea5434aeceb2d
Jon Goodger <jonno.is@gmail.com>
2025-05-06 03:06:48 +0100

    Fully document client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,12 +1,39 @@
+/**
+ * Delay thread for the specified time
+ *
+ * Must be used in an async function and awaited
+ *
+ * ```js
+ * // e.g.
+ * console.log('waiting...');
+ * await delay(300);
+ * console.log('waited!');
+ * ```
+ *
+ * @param ms number of milliseconds to delay for
+ * @returns a promise to await to delay code execution
+ */
 function delay(ms: number): Promise<CitizenTimer> {
     return new Promise(res => setTimeout(res, ms, null));
 }
 
-function isTrueString(x: string) {
+/**
+ * Helper to convert a string convar to a boolean value, looking for true
+ *
+ * @param x the convar to parse
+ * @returns the parsed value
+ */
+function isTrueString(x: string): boolean {
     return ['yes', 'true', 'y', '1'].includes(x.toLowerCase());
 }
 
-function isFalseString(x: string) {
+/**
+ * Helper to convert a string convar to a boolean value, looking for false
+ *
+ * @param x the convar to parse
+ * @returns the parsed value
+ */
+function isFalseString(x: string): boolean {
     return ['no', 'false', 'n', '0', ''].includes(x.toLowerCase());
 }
 
@@ -27,6 +54,12 @@ function getBlipCategoryFromOption(option: string): number {
     return options[option?.toLowerCase()] ?? 7;
 }
 
+/**
+ * Turn the colour convar into an array of numbers
+ *
+ * @param option the convar contents
+ * @returns the option parsed into a list of integers
+ */
 function parseBlipColours(option: string): number[] {
     try {
         const colours = option.split(',')
@@ -57,7 +90,12 @@ const SHOW_SELF = isTrueString(GetConvar('virakal_blips_show_self', 'false'));
 const blips = new Map<number, number>();
 const peds = new Map<number, number>();
 
-function l(...messages: any[]) {
+/**
+ * Call console.log if logging is enabled
+ *
+ * @param messages the messages to log
+ */
+function l(...messages: any[]): void {
     if (!LOGGING_ENABLED) {
         return;
     }
@@ -67,11 +105,26 @@ function l(...messages: any[]) {
     }
 }
 
+/**
+ * Choose a blip colour for the given player
+ *
+ * @param playerId the player ID
+ * @returns a blip colour for the player
+ */
 function getBlipColour(playerId: number): number {
     return BLIP_COLOURS[playerId % BLIP_COLOURS.length];
 }
 
-function createBlip(playerId: number, ped: number): number {
+/**
+ * Create a blip for the given player
+ *
+ * Also records the player's ped so we can detect if it changed later
+ *
+ * @param playerId the ID of the player to create the blip for
+ * @param ped the player's ped
+ * @returns the blip ID
+ */
+function createPlayerBlip(playerId: number, ped: number): number {
     l(`Creating blip for player ${playerId}, ped ${ped}`);
 
     const blip = AddBlipForEntity(ped);
@@ -91,19 +144,37 @@ function createBlip(playerId: number, ped: number): number {
     return blip;
 }
 
-function deleteBlip(playerId: number, blip: number) {
+/**
+ * Remove a player's blip
+ *
+ * @param playerId the player ID to remove the blip for
+ * @param blip the blip to remove
+ */
+function deletePlayerBlip(playerId: number, blip: number): void {
     l(`Deleting blip ${blip} for ${playerId}`);
 
     RemoveBlip(blip);
     blips.delete(playerId);
 }
 
+/**
+ * Check if the player's ped has changed since we last recorded it
+ *
+ * This happens if a player changes to another model, for example.
+ *
+ * @param playerId the player ID to check for
+ * @param ped the player's current ped
+ * @returns whether the ped has changed since we last saw it
+ */
 function pedHasChanged(playerId: number, ped: number): boolean {
     const oldPed = peds.get(playerId);
     return ped !== oldPed;
 }
 
-async function updateBlips() {
+/**
+ * Create and remove blips as needed
+ */
+async function updateBlips(): Promise<void> {
     const playerList = GetActivePlayers();
 
     l('Got player list', playerList);
@@ -116,7 +187,7 @@ async function updateBlips() {
             l(`skipping ${playerId} because its me!`);
 
             if (existingBlip) {
-                deleteBlip(playerId, existingBlip);
+                deletePlayerBlip(playerId, existingBlip);
             }
 
             continue;
@@ -134,16 +205,18 @@ async function updateBlips() {
             && (!existingBlip || pedHasChanged(playerId, ped))
         ) {
             l('Entity exists and we don\'t have a blip for it yet, or ped changed');
-            createBlip(playerId, ped);
+            createPlayerBlip(playerId, ped);
         } else if (existingBlip && !pedExists) {
             l('Entity doesn\'t exist - remove the blip');
-            deleteBlip(playerId, existingBlip);
+            deletePlayerBlip(playerId, existingBlip);
         } else {
             l('Doing nothing');
         }
     }
 
+    // Wait for the delay time to save on server load
     await delay(BLIP_UPDATE_DELAY);
 }
 
+// Run the update blips code on tick
 setTick(updateBlips);
