9e87c7e1b64a8f4bf3855288ac1afc95b1402735
Jon Goodger <jonno.is@gmail.com>
2025-05-06 01:52:32 +0100

    Code cleanup and convar caching

--- client/client.ts
+++ client/client.ts
@@ -22,21 +22,23 @@ function getBlipCategoryFromOption(option: string): number {
         other_player: 7,
         regular_with_map_distance: 2,
         regular_no_map_distance: 1,
-    }
+    };
 
     return options[option?.toLowerCase()] ?? 7;
 }
 
-const BLIP_UPDATE_DELAY = GetConvarInt('virakal_blips_update_ms', 1000);
-const BLIP_SIZE = GetConvarInt('virakal_blips_size_percentage', 100) / 100;
-const BLIP_COLOUR = 25; // green
 const BLIP_CATEGORY = getBlipCategoryFromOption(GetConvar('virakal_blips_style', ''));
+const BLIP_COLOUR = 25; // green
+const BLIP_SIZE = GetConvarInt('virakal_blips_size_percentage', 100) / 100;
+const BLIP_UPDATE_DELAY = GetConvarInt('virakal_blips_update_ms', 1000);
+const LOGGING_ENABLED = isTrueString(GetConvar('virakal_blips_debug_log', 'false'));
+const SHOW_SELF = isTrueString(GetConvar('virakal_blips_show_self', 'false'));
 
-const blips: Map<number, number> = new Map();
-const peds: Map<number, number> = new Map();
+const blips = new Map<number, number>();
+const peds = new Map<number, number>();
 
 function l(...messages: any[]) {
-    if (!isTrueString(GetConvar('virakal_blips_debug_log', 'false'))) {
+    if (!LOGGING_ENABLED) {
         return;
     }
 
@@ -75,14 +77,13 @@ function pedHasChanged(playerId: number, ped: number): boolean {
 
 async function updateBlips() {
     const playerList = GetActivePlayers();
-    const showSelf = isTrueString(GetConvar('virakal_blips_show_self', 'false'));
 
     l('Got player list', playerList);
 
     for (const playerId of playerList) {
         const existingBlip = blips.get(playerId);
 
-        if (!showSelf && playerId === GetPlayerIndex()) {
+        if (!SHOW_SELF && playerId === GetPlayerIndex()) {
             // Don't make a blip for ourselves
             l(`skipping ${playerId} because its me!`);
 
@@ -104,11 +105,9 @@ async function updateBlips() {
             pedExists
             && (!existingBlip || pedHasChanged(playerId, ped))
         ) {
-            // Entity exists and we don't have a blip for it yet, or the ped had changed
             l('Entity exists and we don\'t have a blip for it yet, or ped changed');
             createBlip(playerId, ped);
         } else if (existingBlip && !pedExists) {
-            // Entity doesn't exist - remove the blip
             l('Entity doesn\'t exist - remove the blip');
             deleteBlip(playerId, existingBlip);
         } else {
